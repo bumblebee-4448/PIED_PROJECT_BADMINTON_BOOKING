cd35b77b83f98f0c70cf531ed86fb84ce67e35fa
phantanhungpy20061@gmail.com <phantanhungpy20061@gmail.com>
2026-05-12 20:48:24 +0700

    fix_name_in_Program.cs

--- backend/Rallyhub/Rallyhub.Api/Program.cs
+++ backend/Rallyhub/Rallyhub.Api/Program.cs
@@ -22,9 +22,9 @@ using TransactionService = Rallyhub.Service.Transaction;
 using WalletService = Rallyhub.Service.Wallet;
 using BookingService = Rallyhub.Service.Booking;
 using WithdrawalService = Rallyhub.Service.Withdrawal;
-using sepayService = Rallyhub.Service.SepayService;
+using SepayService = Rallyhub.Service.SepayService;
 using NotificationService = Rallyhub.Service.NotificationService;
-using FeadbackService = Rallyhub.Service.Feadback;
+using FeedbackService = Rallyhub.Service.Feadback;
 
 // using DiscordService = Rallyhub.Service.DiscordService;
 
@@ -69,9 +69,9 @@ builder.Services.AddScoped<TransactionService.IService, TransactionService.Servi
 builder.Services.AddScoped<WalletService.IService, WalletService.Service>();
 builder.Services.AddScoped<BookingService.IService, BookingService.Service>();
 builder.Services.AddScoped<WithdrawalService.IService, WithdrawalService.Service>();
-builder.Services.AddScoped<sepayService.IService, sepayService.Service>();
+builder.Services.AddScoped<SepayService.IService, SepayService.Service>();
 builder.Services.AddScoped<NotificationService.IService, NotificationService.Service>();
-builder.Services.AddScoped<FeadbackService.IService, FeadbackService.Service>();
+builder.Services.AddScoped<FeedbackService.IService, FeedbackService.Service>();
 
 
 
