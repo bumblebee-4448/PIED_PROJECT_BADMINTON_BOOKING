b84b14a5f84b761cdf6fc90d73adc1b4028f09f0
phantanhungpy20061@gmail.com <phantanhungpy20061@gmail.com>
2026-05-15 18:16:57 +0700

    fix full bug3

--- backend/Rallyhub/Rallyhub.Service/Booking/Service.cs
+++ backend/Rallyhub/Rallyhub.Service/Booking/Service.cs
@@ -423,7 +423,28 @@ public class Service: IService
         {
             throw new Exception("Wallet apart balance failed");
         } 
-//transaction
+        //transaction
+        var wallet = await _dbContext.Wallets.FirstOrDefaultAsync(x => x.UserId == userId.Id);
+        if (wallet == null)
+        {
+            throw new Exception("Wallet not found");
+        }
+        var transactionI = new Transaction.Request.CreateTransactionRequest()
+        {
+            Type = Transaction.Request.TypeList.Payment,
+            Amount = finalPrice,
+            BalanceBefore = wallet.Balance + finalPrice, 
+            BalanceAfter = wallet.Balance,           
+            Status = "Success",
+            BookingId = booking.Id,
+            WalletId = wallet.Id,
+        };
+
+        if (!await _transactionService.CreateTransaction(transactionI))
+        {
+            throw new Exception("Error creating transaction");
+        }
+
         booking.Status = "Banked";
         foreach (var item in bookingDetails)
         {
